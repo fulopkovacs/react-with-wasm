e30ca672f0e0dc8d64a15d3e204c65e0b4a3f385
Fülöp Kovács <43729152+fulopkovacs@users.noreply.github.com>
2025-08-31 11:40:07 +0200

    Pre-allocate the memory in wasm and populate it in js for the input of the wasm grayscale conversion function

--- packages/wasm-module/src/lib.rs
+++ packages/wasm-module/src/lib.rs
@@ -15,8 +15,11 @@ pub fn get_arr_length(arr: &[f32]) -> usize {
     arr.len()
 }
 
+// pub fn convert_to_grayscale(ptr: u8, size: usize) -> *const u8 {
+//     let image_data: &[u8] = unsafe { std::slice::from_raw_parts(&ptr as *const u8, size) }; // Assuming max size of 1MB RGBA image
 #[wasm_bindgen]
-pub fn convert_to_grayscale(image_data: &[u8]) -> *const u8 {
+pub fn convert_to_grayscale(ptr: *const u8, size: usize) -> *const u8 {
+    let image_data = unsafe { std::slice::from_raw_parts(ptr, size) };
     let mut grayscale_data = Vec::with_capacity(image_data.len() / 4);
     for chunk in image_data.chunks(4) {
         let r = chunk[0] as f32;
@@ -37,3 +40,24 @@ pub fn convert_to_grayscale(image_data: &[u8]) -> *const u8 {
 // pub fn convert_to_grayscale(image_data: &[u8]) -> Vec<u8> {
 //     image_data.to_vec()
 // }
+
+// https://docs.rs/console_error_panic_hook/latest/console_error_panic_hook/
+// TODO: check if we should call alloc
+// #[wasm_bindgen]
+// pub fn allocate_memory(size: usize) -> *const u8 {
+//     &Vec::with_capacity(size)[0] as *const u8
+
+#[wasm_bindgen]
+pub extern "C" fn alloc(size: usize) -> *mut u8 {
+    let mut buf = Vec::with_capacity(size);
+    let ptr = buf.as_mut_ptr();
+    std::mem::forget(buf); // take ownership of memory block
+    ptr
+}
+
+#[wasm_bindgen]
+pub unsafe extern "C" fn dealloc(ptr: *mut u8, size: usize) {
+    let align = std::mem::align_of::<usize>();
+    let layout = std::alloc::Layout::from_size_align_unchecked(size, align);
+    std::alloc::dealloc(ptr, layout);
+}
